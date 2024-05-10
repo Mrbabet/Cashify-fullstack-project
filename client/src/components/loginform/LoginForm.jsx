3c82443fd5ce8ddccf6e979c4a1a5ff094111dd5
Wiktoria Skibińska <wikas2000@icloud.com>
2024-05-10 16:09:45 +0200

    tablet and desktop versions

--- client/src/components/loginform/LoginForm.jsx
+++ client/src/components/loginform/LoginForm.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "../../components/loginform/LoginForm.module.css";
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const isEmailInvalid = !email && isSubmitted;
+  const isPasswordInvalid = !password && isSubmitted;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+  };
+
   return (
     <section className={css.loginSection}>
       <div className={css.wrapper}>
@@ -10,19 +27,40 @@ const LoginForm = () => {
             <p className={css.paragraph}>
               You can log in with your Google Account:
             </p>
-            <button className={css.googleBttn}>Google</button>
+            <button className={css.googleBttn}>
+              <div className={css.googlesymbol}></div>
+              <span>Google</span>
+            </button>
             <p className={css.paragraph}>
               Or log in using an email and password, after registering:
             </p>
           </div>
-          <form className={css.form} autoComplete="off">
+          <form className={css.form} autoComplete="off" onSubmit={handleSubmit}>
             <label className={css.label}>
               Email:
-              <input type="email" name="email" placeholder="your@email.com" />
+              <input
+                className="emailinput"
+                type="email"
+                name="email"
+                placeholder="your@email.com"
+                onChange={handleEmailChange}
+              />
+              {isEmailInvalid && (
+                <span className={css.requiredSpan}>This field is required</span>
+              )}
             </label>
             <label className={css.label}>
               Password:
-              <input type="password" name="password" placeholder="Password" />
+              <input
+                className="passwordinput"
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handlePasswordChange}
+              />
+              {isPasswordInvalid && (
+                <span className={css.requiredSpan}>This field is required</span>
+              )}
             </label>
             <div className={css.bttnsDiv}>
               <button type="submit">LOG IN</button>
