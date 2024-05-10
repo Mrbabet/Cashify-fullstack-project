3c1c526a49c6a3b03c643b63ad1591d147fa3e71
Mrbabet <wieliczkokamil@gmail.com>
2024-05-10 16:58:33 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import LoginForm from "./components/loginform/LoginForm";
 function App() {
   return (
     <>
-      <div>tu można wstawiać swoje pages/ komponenty</div>
       <LoginForm />
     </>
   );
+}
 
 export default App;
