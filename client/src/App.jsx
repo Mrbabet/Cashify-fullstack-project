ba0de3fae8990b887e3c987fb1dc2240f6b33a73
Wiktoria Skibińska <wikas2000@icloud.com>
2024-05-12 23:01:41 +0200

    register page, routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import LoginForm from "./components/loginform/LoginForm";
+import RegisterForm from "./components/registerform/RegisterForm";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <LoginForm />
-    </>
+    <Routes>
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+    </Routes>
   );
 }
 
---
+++ client/src/components/registerform/RegisterForm.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import css from "../../components/registerform/RegisterForm.module.css";
+const RegisterForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const isEmailInvalid = !email && isSubmitted;
+  const isPasswordInvalid = !password && isSubmitted;
+  const isUsernameInvalid = !username && isSubmitted;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+  };
+
+  return (
+    <section className={css.registerSection}>
+      <div className={css.wrapper}>
+        <div className={css.logo}></div>
+        <div className={css.formContainer}>
+          <div className={css.paragraphDiv}>
+            <p className={css.paragraph}>
+              You can register with your Google Account:
+            </p>
+            <button className={css.googleBttn}>
+              <div className={css.googlesymbol}></div>
+              <span>Google</span>
+            </button>
+            <p className={css.paragraph}>Or fill the fields below: </p>
+          </div>
+          <form className={css.form} autoComplete="off" onSubmit={handleSubmit}>
+            <label className={css.label}>
+              Username:
+              <input
+                className="usernameinput"
+                type="text"
+                name="username"
+                placeholder="Your username"
+                onChange={handleUsernameChange}
+              />
+              {isUsernameInvalid && (
+                <span className={css.requiredSpan}>This field is required</span>
+              )}
+            </label>
+            <label className={css.label}>
+              Email:
+              <input
+                className="emailinput"
+                type="email"
+                name="email"
+                placeholder="your@email.com"
+                onChange={handleEmailChange}
+              />
+              {isEmailInvalid && (
+                <span className={css.requiredSpan}>This field is required</span>
+              )}
+            </label>
+            <label className={css.label}>
+              Password:
+              <input
+                className="passwordinput"
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handlePasswordChange}
+              />
+              {isPasswordInvalid && (
+                <span className={css.requiredSpan}>This field is required</span>
+              )}
+            </label>
+            <div className={css.bttnsDiv}>
+              <button type="submit" className={css.active}>
+                REGISTER
+              </button>
+              <button type="button">LOGIN</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RegisterForm;
