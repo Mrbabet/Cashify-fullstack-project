b20f2cca001209ee37dcb95ba33d0cfcd7903f0e
DariuszCedro <dariuszcedro@gmail.com>
2024-05-13 19:38:55 +0200

    Balance component stylization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import LoginForm from "./components/loginform/LoginForm";
+import Balance from "./components/Balance/Balance";
 
 function App() {
   return (
     <>
       <LoginForm />
+      {/* <Balance /> */}
+
     </>
   );
 }
