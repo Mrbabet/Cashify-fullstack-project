c390e2cda1f730eceffa066cec7a981db3646b91
DariuszCedro <dariuszcedro@gmail.com>
2024-05-15 14:39:08 +0200

    Transactions styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,44 @@
 import Layout from "./components/Layout/Layout";
 import LoginForm from "./components/loginform/LoginForm";
 import RegisterForm from "./components/registerform/RegisterForm";
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AfterRegister from "./components/afterRegister/AfterRegister.jsx";
+import PrivateRoute from "./components/PrivateRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AfterRegisterRoute from "./components/AfterRegisterRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />'
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={<PrivateRoute Component={<LoginForm />} redirecTo="/home" />}
+        />
+
+        <Route
+          path="register"
+          element={
+            <AfterRegisterRoute
+              Component={<RegisterForm />}
+              redirecTo="/afterregister"
+            />
+          }
+        />
+        <Route
+          path="home"
+          element={<ProtectedRoute Component={<Home />} redirecTo="/" />}
+        />
+        <Route
+          path="afterregister"
+          element={
+            <ProtectedRoute
+              Component={<AfterRegister />}
+              redirecTo="/register"
+            />
+          }
+        />
       </Route>
     </Routes>
   );
