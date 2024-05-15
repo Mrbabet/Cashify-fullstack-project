b02b9b0d783d2062f670dd85dbfe2860ae05cb77
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2024-05-15 11:37:06 +0200

    Revert "mobile"

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,16 +1,17 @@
-import PropTypes from "prop-types";
-import { Outlet } from "react-router-dom";
-import Header from "../header/Header";
-import Background from "../background/Background";
+import PropTypes from 'prop-types'; 
+import { Outlet } from 'react-router-dom';
+import Header from '../header/Header';
+import Background from '../background/Background';
 
 const Layout = ({ isAuthenticated }) => {
   return (
     <Background isAuthenticated={isAuthenticated}>
-      <Header />
-      <Outlet />
+      <Header />      
+        <Outlet />     
     </Background>
   );
 };
+
 
 Layout.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,24 +1,9 @@
 import css from "../../components/header/Header.module.css";
-import { Avatar } from "@chakra-ui/react";
-
 const Header = () => {
   return (
-    <header>
-      <div className={css.headerContainer}>
-        <div className={css.headerLogo}></div>
-        <div className={css.userLogout}>
-          <div className={css.user}>
-            <Avatar
-              name="User"
-              src="https://bit.ly/broken-link"
-              borderRadius="50%"
-              backgroundColor="lightgrey"
-              height="32px"
-              width="32px"
-            />
-          </div>
-          <div className={css.logout}></div>
-        </div>
+    <header className={css.headerContainer}>
+      <div className={css.header}>
+        <div className={css.headerlogo}></div>
       </div>
     </header>
   );
