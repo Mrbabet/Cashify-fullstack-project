63cad89b9e13337a5b8ef2aefccaf30715ee4d16
Mrbabet <wieliczkokamil@gmail.com>
2024-05-19 19:39:30 +0200

    added 404

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  server: {
+    // Handle fallback for history API routing
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      external: ["redux-persist/integration/react"],
+    },
+  },
+});
